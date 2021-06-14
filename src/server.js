4840a1110c6e74ddbddc45a799a9ec7fa3e00ce2
Rizky Prayatman <67992434+rizkyprayatman@users.noreply.github.com>
2021-06-14 12:35:19 +0800

    Update serves.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable no-unused-vars */
 /* eslint-disable indent */
 
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/NotesService');
+const NotesValidator = require('./validator/notes');
 
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.39.16',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
@@ -15,7 +20,13 @@ const init = async () => {
     },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
