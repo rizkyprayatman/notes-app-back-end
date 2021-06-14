865be5e8799c5c10ecedb85bc08c5416e1f76bb1
Rizky Prayatman <r.prayatman@gmail.com>
2021-06-14 12:31:49 +0800

    data validation implemented

---
+++ src/validator/notes/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+
+const InvariantError = require('../../exceptions/InvariantError');
+const { NotePayloadSchema } = require('./schema');
+
+const NotesValidator = {
+  validateNotePayload: (payload) => {
+    const validationResult = NotePayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = NotesValidator;
---
+++ src/validator/notes/schema.js
@@ -0,0 +1,13 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line linebreak-style
+
+const Joi = require('joi');
+
+const NotePayloadSchema = Joi.object({
+  title: Joi.string().required(),
+  body: Joi.string().required(),
+  tags: Joi.array().items(Joi.string()).required(),
+});
+
+module.exports = { NotePayloadSchema };
